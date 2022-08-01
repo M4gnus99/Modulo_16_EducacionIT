a3eeb58d5bc5928f689cb0e66255ed781d99d60a
M4gnus99 <beaterquinteros@gmail.com>
2022-07-31 22:27:23 -0300

    sin terminar

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
-let Producto = (id, nombre, precio, stock) => {
+let Producto = function(id, nombre, precio, stock) {
     return {
         id: id,
         nombre: nombre,
         precio: precio,
         stock: stock
+    }
+}
+
+let Carrito = function() {
+
+    let items = [];
+
+    function agregar (producto){
+        items.push({producto: producto})
+        if(items.length >= 2){
+            console.log("Tiene algo dentro");
+        }
+    }
+    function reporte() {
+
+        let detalle = items.map(function(val){
+            return {
+                nombre: val.producto.nombre,
+                precioUnitario: val.producto.precio,
+                cantidad: val.cantidad,
+                total: val.producto.precio
+            }
+        });
+        return {
+            detalle: detalle
+        };
+    }
+
+
+    return {
+        agregar: agregar,
+        reporte: reporte
     }
 }
